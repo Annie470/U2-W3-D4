6e1daa57d8602d6693eeb3d281640e9c4fa40d6f
Anna De Lise <anna_delise@libero.it>
2025-07-10 16:39:55 +0200

    add last exercise without extra

--- details.js
+++ details.js
@@ -1,6 +1,31 @@
 const parameters = new URLSearchParams(location.search);
-const endpoint = `https://api.pexels.com/v1/search?query=`;
+const endpoint2 = `https://api.pexels.com/v1/photos/`;
 const eventId = parameters.get(`eventId`);
 
 console.log(location.search);
 console.log(eventId);
+
+//chiamata con id
+fetch(endpoint2 + eventId, {
+  headers: {
+    Authorization: "kT54yxGJQw0pIHLnBAXnCZaqyhx31Hq8Cxhfq7O3TuoHaC254kgDdPn5",
+  },
+})
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`errore`);
+    }
+  })
+  .then((data) => {
+    const img = document.querySelector("img");
+    img.setAttribute("src", data.src.medium);
+    const h5 = document.querySelector("h5");
+    h5.innerText = data.photographer;
+    const anch = document.getElementById("special-a");
+    anch.setAttribute("href", data.photographer_url);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
